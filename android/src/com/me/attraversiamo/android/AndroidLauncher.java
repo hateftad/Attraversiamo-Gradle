6f09e93347978cecf3426db9ac95e9764e9a7014
Hatef Tadayon <hatef.tadayon@gmail.com>
2014-04-16 19:48:23 +0200

    Android build changes

--- android/src/com/me/attraversiamo/android/AndroidLauncher.java
+++ android/src/com/me/attraversiamo/android/AndroidLauncher.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.me.attraversiamo.Attraversiamo;
+import com.me.utils.GameConfig;
+import com.me.utils.GameConfig.Platform;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Attraversiamo(), config);
+		
+		GameConfig cfg = new GameConfig();
+		cfg.platform = Platform.ANDROID;
+		cfg.timeStep = 1/60f;
+		cfg.showUI = true;
+		cfg.zoom = 5f;
+		
+		initialize(new Attraversiamo(cfg), config);
 	}
 }
